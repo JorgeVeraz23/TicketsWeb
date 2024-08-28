97d8566bc1e9176d45a7f80e13a34146bfa42146
Jorge Vera Zambrano <125833333+JorgeVeraz23@users.noreply.github.com>
2024-08-28 11:12:45 -0500

    Update App.jsx

--- TicketsProject/TicketsWeb/src/App.jsx
+++ TicketsProject/TicketsWeb/src/App.jsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [solicitudes, setSolicitudes] = useState([]);
+
+  useEffect(() => {
+    //Llamada a la API para obtener las solicitudes
+    fetch("https://localhost:7016/api/Solicitud/GetSolicitudesDeUsuario?idUsuario=1")
+    .then((response) => response.json())
+    .then((data) => setSolicitudes(data))
+    .catch((error) => console.error('Error al obtener las solicitudes: '+ error));
+  }, []);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {solicitudes.map((solicitud) => (
+        <div key={solicitud.idSolicitud}>
+          <h3>{solicitud.tipoSolicitud}</h3>
+          <p>{solicitud.descripcionSolicitud}</p>
+          <img 
+            src={`https://localhost:7016/${solicitud.justificativo.replace(/\\/g, '/')}`} 
+            alt="Justificativo" 
+            style={{ width: '300px', height: 'auto' }} 
+          />
+          <p>{solicitud.detalleGestion}</p>
+          <p>{new Date(solicitud.fechaIngreso).toLocaleString()}</p>
+        </div>
+      ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
